9eceb72f69bc4665c283513f64b8e235fbed2d06
unik89 <168553624+unik89@users.noreply.github.com>
2025-02-08 09:51:04 +0530

    project

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask,render_template,request 
-app=Flask(__name__)
+from flask import Flask, render_template,request,send_from_directory
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/vid/<path:filename>')
+def serve_video(filename):
+    return send_from_directory('vid', filename)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
-@app.route("/predict",methods=["POST","Get"])
+@app.route("/predict",methods=["POST","GET"])
 def predict():
     if request.method=="POST":
         msg=request.form.get("message")
@@ -12,5 +20,6 @@ def predict():
     else:
         return render_template("predict.html")
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5054)
+
+if __name__ =="__main__":
+    app.run(host="0.0.0.0", port=5050)
