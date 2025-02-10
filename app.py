e29c6c29d516359dbbbb8fa546e46a5f0b71b8ce
unik89 <168553624+unik89@users.noreply.github.com>
2025-02-10 10:25:28 +0530

    `Updated code for text to number prediction with Flask and pickle files`

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
-from flask import Flask, render_template,request,send_from_directory
-from flask_cors import CORS
+from flask import Flask,render_template,request
+from test import TextToNum
+import pickle
 
-app = Flask(__name__)
-CORS(app)
 
-@app.route('/vid/<path:filename>')
-def serve_video(filename):
-    return send_from_directory('vid', filename)
-
+app=Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
-@app.route("/predict",methods=["POST","GET"])
+@app.route("/predict",methods=["POST","Get"])
+
 def predict():
     if request.method=="POST":
         msg=request.form.get("message")
         print(msg)
+        ob=TextToNum(msg)
+        ob.cleaner()
+        ob.token()
+        ob.removeStop()
+        st=ob.stemme()
+        stem_vector=" ".join(st)
+
+        with open("vectorizer.pickle","rb") as vc:
+            vectorizer=pickle.load(vc)
+        vcdata=vectorizer.transform([stem_vector]).toarray()
+        print(vcdata)
+        
+        with open("model.pickle","rb") as mc:
+            model=pickle.load(mc)
+
+        pred=model.predict(vcdata)
+        print(pred)
+        
     else:
         return render_template("predict.html")
 
-
-if __name__ =="__main__":
-    app.run(host="0.0.0.0", port=5050)
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=5050)
