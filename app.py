41c3ca712d8ab8c4c63ff4bedbc196b20aecd4fa
SAIDHARMENDRA V <vv92343@gmail.com>
2025-02-14 12:38:10 +0530

    final commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, jsonify, url_for
+from flask import Flask, request, render_template, jsonify
 from scripts.predict import predict_sentiment  # Your prediction function
 
 app = Flask(__name__, static_folder='static')
@@ -7,14 +7,17 @@ app = Flask(__name__, static_folder='static')
 def index():
     return render_template('index.html')
 
+
+
+
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
         user_input = request.form.get('message', '')
         prediction = predict_sentiment(user_input)
         return jsonify({'prediction': prediction})
+    return render_template('predict.html')  # Handles GET requests
 
-    return render_template('predict.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0" , port='5050')
